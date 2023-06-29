1fcffd3fcc4b20d2d5d748750bb2692a8bb79592
Kirill Nizhnik <kiril.niznik@gmail.com>
2023-06-29 14:28:55 +0300

    HI

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,8 @@
-function show(){
-    document.getElementById('sidebar').
-    classList.toggle('active')
+function toggleMenu() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.style.display === "none") {
+        dropdownMenu.style.display = "block";
+    } else {
+        dropdownMenu.style.display = "none";
+    }
 }
