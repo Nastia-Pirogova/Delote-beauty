94f68bc60e1afb629eb6c2d94ac9497c535c5884
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-06-29 16:00:17 +0300

    :+1:

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,8 +1,32 @@
-function toggleMenu() {
-    var dropdownMenu = document.getElementById("dropdownMenu");
-    if (dropdownMenu.style.display === "none") {
-        dropdownMenu.style.display = "block";
-    } else {
-        dropdownMenu.style.display = "none";
-    }
+const refs = {
+    menuBtn: document.getElementById("menuBtn"),
+    menuContainer: document.getElementById("js-menu-container"),
+    btnCloseMenu: document.getElementById("btnCloseMenu"),
 }
+
+refs.menuBtn.addEventListener('click', toggleMenu);
+refs.btnCloseMenu.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    const isMenuOpen =
+    refs.menuBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.menuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    refs.menuContainer.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+}
+
+
+
+
+// function toggleMenu() {
+//     var dropdownMenu = document.getElementById("dropdownMenu");
+//     if (dropdownMenu.style.display === "none") {
+//         dropdownMenu.style.display = "block";
+//     } else {
+//         dropdownMenu.style.display = "none";
+//     }
+// }
